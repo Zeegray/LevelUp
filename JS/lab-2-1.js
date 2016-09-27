3abff9ece600b5b420fe397f8e5f6f3e5801ad8e
Zeegray <zeegrain545@gmail.com>
2016-09-27 12:43:49 +0300

    lab-2-1(3)

--- JS/lab-2-1.js
+++ JS/lab-2-1.js
@@ -1,9 +1,17 @@
-var num = 2;
-var exp = 10;
-var result = 1;
-var cnt = 1;
-while (cnt <= exp) {
-    result *= num;
-    cnt++;
-}
-console.log(result);
+var a = prompt("Введите возраст");
+if (a > 0 && a <= 3) {
+    console.log("Младенец")
+} else if (a > 3 && a <= 12) {
+    console.log("Ребенок");
+} else if (a > 12 && a <= 16) {
+    console.log("Подросток");
+} else if (a > 16 && a <= 21) {
+    console.log("Молодец");
+} else if (a > 21 && a < 55) {
+    console.log("Мужчина/Женщина среднего возраста");
+} else if (a >= 55 && a < 60) {
+    console.log("Женщина пенсионер");
+    console.log("Мужчина среднего возраста");
+} else if (a >= 60) {
+    console.log("Мужчина/Женщина пенсионер");
+} 
